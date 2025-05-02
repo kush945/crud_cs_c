ccf6946767f8f9dd16536247dc519cf5f859f232
kush945 <lavkushsklmaurya@gmail.com>
2025-05-02 15:26:50 +0530

    ok

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,12 @@
-const mongoose =require('mongoose');
-async function dbConn(){
-    const conn=await mongoose.connect('mongodb://localhost:27017/studentapp');
-    if(conn){
+const mongoose = require('mongoose');
+async function dbConn() {
+    const conn = await mongoose.connect('mongodb+srv://kush:root@cluster0.fxeswa2.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0');
+    if (conn) {
         console.log('database connect successfully')
 
     }
-    else{
+    else {
         console.log('connection fail');
     }
 }
-module.exports=dbConn;
+module.exports = dbConn;
